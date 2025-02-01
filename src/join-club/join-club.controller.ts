abea11d7d7acc6a25d6852986ac4c396528ce1a2
romss1 <romanberth@gmail.com>
2025-02-01 19:31:09 +0100

    accept or reject request to join club

--- src/join-club/join-club.controller.ts
+++ src/join-club/join-club.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
 import { JoinClubService } from './join-club.service';
 import { Auth } from 'src/auth/auth.decorator';
 import { Role, User } from '@prisma/client';
@@ -16,5 +16,25 @@ export class JoinClubController {
         @UserDecorator() { id }: User
     ): Promise<void> {
         return this.joinClubRequestService.requestToJoinClub(createJoinClubRequestDto, id);
+    }
+
+    // TODO Ajouter le check: update seulement si le status = Pending
+    // TODO: Avoir un Role CLUB_ADMIN
+    @Auth(Role.USER)
+    @Patch(':id/accept')
+    async acceptJoin(
+        @Param('id') id: string
+    ): Promise<void> {
+        return this.joinClubRequestService.acceptToJoinClub(id);
+    }
+
+    // TODO Ajouter le check: update seulement si le status = Pending
+    // TODO: Avoir un Role CLUB_ADMIN
+    @Auth(Role.USER)
+    @Patch(':id/reject')
+    async rejectJoin(
+        @Param('id') id: string
+    ): Promise<void> {
+        return this.joinClubRequestService.rejectToJoinClub(id);
     }
 }
--- src/join-club/join-club.service.ts
+++ src/join-club/join-club.service.ts
@@ -1,18 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { CreateJoinClubRequestDTO } from './dto/create.join.club.request.dto';
 import { PrismaService } from 'src/prisma.service';
+import { JoinClubModule } from './join-club.module';
+import { JoinClubRequestStatus } from '@prisma/client';
 
 @Injectable()
 export class JoinClubService {
-    constructor(private readonly prisma: PrismaService){}
+    constructor(private readonly prisma: PrismaService) { }
 
     async requestToJoinClub(createJoinClubRequestDTO: CreateJoinClubRequestDTO, userId: string): Promise<void> {
-        await this.prisma.clubJoinRequest.create({ 
+        await this.prisma.clubJoinRequest.create({
             data: {
                 userId: userId,
                 clubId: createJoinClubRequestDTO.clubId,
                 message: createJoinClubRequestDTO.message
-            } 
+            }
+        })
+    }
+
+    async acceptToJoinClub(joinClubRequestId: string): Promise<void> {
+        await this.prisma.clubJoinRequest.update({
+            where: {
+                id: joinClubRequestId
+            },
+            data: {
+                status: JoinClubRequestStatus.APPROVED
+            }
+        })
+    }
+
+    async rejectToJoinClub(joinClubRequestId: string): Promise<void> {
+        await this.prisma.clubJoinRequest.update({
+            where: {
+                id: joinClubRequestId
+            },
+            data: {
+                status: JoinClubRequestStatus.REJECTED
+            }
         })
     }
 }
